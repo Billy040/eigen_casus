9cb2300ef748226a48973e1dca0e9b3929e03a4f
Billy Dekkers <ps237376@edu.summacollege.nl>
2026-09-16 10:42:49 +0200

    initial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        DB::table('company')->insert([
+            [
+                'name' => 'Tech Solutions Inc.',
+                'email' => 'TechSolutions@gmail.com']
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('internship_position')->insert([
+            [
+                'name' => 'Software Development Intern',
+                'description' => 'Assist in developing and maintaining software applications.',
+                'available' => true,
+                'company_id' => 1
+            ],
+            [
+                'name' => 'Data Analysis Intern',
+                'description' => 'Support data analysis and reporting tasks.',
+                'available' => true,
+                'company_id' => 1
+            ],
+            [
+                'name' => 'Marketing Intern',
+                'description' => 'Help with marketing campaigns and social media management.',
+                'available' => true,
+                'company_id' => 1
+            ]
         ]);
     }
 }
