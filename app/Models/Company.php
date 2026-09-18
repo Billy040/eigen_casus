7673119b52a2eecab108d216a267b2f6e93e4847
Billy Dekkers <ps237376@edu.summacollege.nl>
2026-09-18 12:14:19 +0200

    defined relation in models

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    public function internshipPositions()
+    {
+        return $this->hasMany(Internship_position::class);        
+    }
 }
--- app/Models/Internship_position.php
+++ app/Models/Internship_position.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Internship_position extends Model
 {
-    //
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
