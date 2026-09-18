74cfc023e7876dc7656c5d698df29f8be37adc6e
Billy Dekkers <ps237376@edu.summacollege.nl>
2026-09-18 12:27:17 +0200

    added fillables to my models

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
+    protected $fillable = ['name', 'email'];
+
     public function internshipPositions()
     {
         return $this->hasMany(Internship_position::class);        
--- app/Models/Internship_position.php
+++ app/Models/Internship_position.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Internship_position extends Model
 {
+    protected $fillable = ['name', 'description', 'available', 'company_id'];
+
     public function company()
     {
         return $this->belongsTo(Company::class);
